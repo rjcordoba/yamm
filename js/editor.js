dbaa793a8f7cea0a32dab2fd8d5d967ed4598de8
rjcordoba <rcordoba77@yahoo.es>
2019-08-14 00:12:09 +0200

    Corrección error primera línea

--- js/editor.js
+++ js/editor.js
@@ -3,19 +3,20 @@ var $qS = document.querySelector;
 var $bId = document.getElementById;
 class Editor{
     constructor() {
-	document.execCommand("defaultParagraphSeparator", false, "p");
 
 	const pizarra = document.getElementById("pizarra");
 	const cnúmeros = document.querySelector("#pantalla > div:first-child");
 	const cpizarra = document.querySelector("#pantalla > div:last-child");
 	const números = document.getElementById("nums");
 	var unum = 1;
-	
+
+	// Inicializar la pizarra
 	pizarra.contentEditable = true;
 	pizarra.spellcheck = false;
 	números.innerHTML = "<li>1</li>";
 	pizarra.innerHTML = "<p></p>";
-
+	document.execCommand("defaultParagraphSeparator", false, "p");
+	
 	const observador = new MutationObserver(e => {
 	    if(e.length == 1)
 		if(e[0].addedNodes.length > 0) añadir_num();
@@ -25,7 +26,7 @@ class Editor{
 	observador.observe(pizarra, {childList: true});
 
 	cpizarra.addEventListener("scroll", () => {
-	    cnúmeros.scrollTop = cpizarra.scrollTop; console.log(cpizarra.scrollTop, cnúmeros.scrollTop);}, false);
+	    cnúmeros.scrollTop = cpizarra.scrollTop;}, false);
 
 	const num_lista = document.createElement("li");
 	function añadir_num(){
@@ -33,39 +34,27 @@ class Editor{
 	    el.textContent = ++unum;
 	    números.appendChild(el);
 	}
+
+	const pclick = new MouseEvent('click');
+	const primera_línea = document.createElement("p");
+	primera_línea.addEventListener("click", (e) => {
+	    console.log(e);}, false);
 	function quitar_num(){
-	    if(unum == 1) pizarra.innerHTML = "<p></p>";
-	    else{
-		unum--;
-		números.lastChild.remove();
+	    unum--;
+	    números.lastChild.remove();
+	    if(unum == 0){
+		pizarra.appendChild(primera_línea);
+		document.getSelection().collapse(primera_línea);
 	    }
 	}
 
+
+
+pizarra.addEventListener('insert', function (e) {console.log("lalala") }, false);
+
+
+
     }
-
-	//this.iniciar_pantalla();
-
-	/*piz.addEventListener("click", e => {
-	    this.probar()}, false);
-
-	piz.addEventListener("cut", e => {
-	    console.log("cut")}, false);
-
-	piz.addEventListener("focus", e => {
-	    console.log("focus")}, false);
-	
-	piz.addEventListener("select", e => {
-	    console.log(e)}, false);*/
-
-	/*piz.addEventListener("mousedown", e => {
-	     console.log(e)}, false);*/
-
-/*    iniciar_pantalla(){
-	this.pizarra.innerHTML = "<p></p>";
-	this.números.innerHTML = "<li>1</li>"
-    }*/
-    //probar(){console.log("lalala");}
-
 }
 
 var editor = new Editor;
