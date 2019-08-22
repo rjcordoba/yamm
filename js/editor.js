044b7212301c3cd40bca5ca4516658e153e773d9
rjcordoba <rcordoba77@yahoo.es>
2019-08-22 16:55:27 +0200

    Leer mconfig inicial.

--- js/editor.js
+++ js/editor.js
@@ -1,162 +1,152 @@
 var nave = document.querySelector("textarea");
-var $qS = document.querySelector;
-var $bId = document.getElementById;
 
-class Editor{
- constructor()
-{
+const pizarra = document.getElementById("pizarra");
+const cont_números = document.querySelector("#pantalla > div:first-child");
+const cont_pizarra = document.querySelector("#pantalla > div:last-child");
+const números = document.getElementById("nums");
+var unum = 1;
+var línea_actual;
 
-   const pizarra = document.getElementById("pizarra");
-	const cont_números = document.querySelector("#pantalla > div:first-child");
-	const cont_pizarra = document.querySelector("#pantalla > div:last-child");
-	const números = document.getElementById("nums");
-	var unum = 1;
-	var línea_actual;
+/*============================
+ *  Inicializar la pizarra
+ *============================*/
 
-	/*============================
-	 *  Inicializar la pizarra
-	 *============================*/
+const primera_línea = document.createElement("p");
+números.innerHTML = "<li>1</li>";
+pizarra.contentEditable = true;
+pizarra.spellcheck = false;
+pizarra.appendChild(primera_línea);
+document.execCommand("defaultParagraphSeparator", false, "p");
+línea_actual = pizarra.firstChild;
 
-	const primera_línea = document.createElement("p");
-	números.innerHTML = "<li>1</li>";
-	pizarra.contentEditable = true;
-	pizarra.spellcheck = false;
-   pizarra.appendChild(primera_línea);
-	document.execCommand("defaultParagraphSeparator", false, "p");
-	línea_actual = pizarra.firstChild;
-	
-	/*====================
-	 *  Observador
-	 *====================*/
+/*====================
+ *  Observador
+ *====================*/
 
-	const num_lista = document.createElement("li");
-		  new MutationObserver(lerroak => {
-		 lerroak.forEach(e => {
-			  if(e.addedNodes.length > 0){
-					var el = num_lista.cloneNode();
-					el.textContent = ++unum;
-					números.appendChild(el);
-			  }
-			  else{
-					unum--;
-					números.lastChild.remove();
-					if(unum == 0){
-						 pizarra.appendChild(primera_línea);
-						 document.getSelection().collapse(primera_línea);
-					}
-			  }
-			  poner_actual();
-		 });
-	}).observe(pizarra, {childList: true});
-
-	function buscar_p(e){
-		 var e = document.getSelection().anchorNode;
-	    while(e.nodeName != "P") e = e.parentNode;
-	    return e;	    
-	}
-
-   function poner_actual(){
-		 línea_actual = buscar_p(document.getSelection().anchorNode);
-	}
-
-	/*====================
-	 *  Manejadores
-	 *====================*/
-
-	 function poner_texto(t){
-		  pizarra.removeEventListener("input", metido_char, false);
-        const texto = t.getData("text").replace(/[\t\v\f]/g, " ").split(/\r\n|\r|\n/);
-        const cantidad =texto.length;
-        document.execCommand("insertText", false, texto[0]);
-		  for(let i=1; i<cantidad; i++){
-				document.execCommand("insertParagraph", false);
-				document.execCommand("insertText", false, texto[i]);
+const num_lista = document.createElement("li");
+new MutationObserver(lerroak => {
+	 lerroak.forEach(e => {
+		  if(e.addedNodes.length > 0){
+				var el = num_lista.cloneNode();
+				el.textContent = ++unum;
+				números.appendChild(el);
 		  }
-		  pizarra.addEventListener("input", metido_char, false);
-	 }
-	
-//	Para que los números de línea hagan scroll con la pizarra.
-	cont_pizarra.addEventListener("scroll", () => {
-	    cont_números.scrollTop = cont_pizarra.scrollTop;}, false);
-
-	{let pul = false;
-	pizarra.addEventListener("focus", () => {
-		 if(!pul) línea_actual = primera_línea;}, false);
-
-	pizarra.addEventListener("mousedown", () => {
-		 pul = true;}, false);
-		  
-	pizarra.addEventListener("mouseup", e => {
-		 if(e.target == pizarra) línea_actual = pizarra.lastChild;
-		 else línea_actual = buscar_p(e.target);e.stopPropagation();}, false);
-
-    pizarra.addEventListener("blur", () => {
-	      pul = false;}, false);
-
-	//  document.querySelector("main section:last-child").addEventListener("mouseup", e => {
-	//  	  pul = true; línea_actual = pizarra.lastChild; console.log(línea_actual,"section");}, false);
-   }
-
-	pizarra.addEventListener("paste", e => {
-	    e.preventDefault();
-		 poner_texto(e.clipboardData || window.clipboardData);}, false);
-
-   pizarra.addEventListener("dragover", e => {
-		 e.preventDefault();}, false);
-
-	cont_pizarra.addEventListener("drop", e => {
-		 e.preventDefault();
-		 pizarra.focus();
-		 poner_texto(e.dataTransfer);}, false);
-
-    
-
-    {let pintro = false, pback = false;
-
-	 function metido_char(){
-		  console.log("input",línea_actual);
-		  if(printro) return;
-		  const texto_línea = línea_actual.textContent;
-		  if(pback && !texto_línea) return;
-	 } 
-
-	 pizarra.addEventListener("keydown", e => {
-		  switch (e.key){
-		  case "Enter":
-				pintro = true;
-				break;
-		  case "Backspace":
-				pback = true;
+		  else{
+				unum--;
+				números.lastChild.remove();
+				if(unum == 0){
+					 pizarra.appendChild(primera_línea);
+					 document.getSelection().collapse(primera_línea);
+				}
 		  }
-	 }, false);
+		  poner_actual();
+	 });
+}).observe(pizarra, {childList: true});
 
-	 pizarra.addEventListener("input", metido_char, false);
-
-	 pizarra.addEventListener("keyup", e => {
-	 	  if(e.key.indexOf("Arrow") != -1) {poner_actual();}
-		  else
-				switch (e.key){
-				case "PageDown":
-					 línea_actual = pizarra.lastChild;
-					 break;
-				case "PageUp":
-					 línea_actual = primera_línea;
-					 break;
-				case "Enter":
-					 pintro = false;
-					 //Comprobar si hay que indentar la línea. La línea anterior la analiza en input.
-					 break;
-				case "Backspace":
-					 pback = false;
-					 break;					 
-		  }
-		  console.log(línea_actual,e);}, false);
-	 }
-
-
-
-	 
-  }
+function buscar_p(e){
+	 var e = document.getSelection().anchorNode;
+	 while(e.nodeName != "P") e = e.parentNode;
+	 return e;	    
 }
 
-var editor = new Editor;
+function poner_actual(){
+	 línea_actual = buscar_p(document.getSelection().anchorNode);
+}
+
+/*====================
+ *  Manejadores
+ *====================*/
+
+function poner_texto(t){
+	 pizarra.removeEventListener("input", metido_char, false);
+	 const texto = t.getData("text").replace(/[\t\v\f]/g, " ").split(/\r\n|\r|\n/);
+	 const cantidad =texto.length;
+	 document.execCommand("insertText", false, texto[0]);
+	 for(let i=1; i<cantidad; i++){
+		  document.execCommand("insertParagraph", false);
+		  document.execCommand("insertText", false, texto[i]);
+	 }
+	 pizarra.addEventListener("input", metido_char, false);
+}
+
+//	Para que los números de línea hagan scroll con la pizarra.
+cont_pizarra.addEventListener("scroll", () => {
+	 cont_números.scrollTop = cont_pizarra.scrollTop;}, false);
+
+{let pul = false;
+ pizarra.addEventListener("focus", () => {
+	  if(!pul) línea_actual = primera_línea;}, false);
+
+ pizarra.addEventListener("mousedown", () => {
+	  pul = true;}, false);
+
+ pizarra.addEventListener("mouseup", e => {
+	  if(e.target == pizarra) línea_actual = pizarra.lastChild;
+	  else línea_actual = buscar_p(e.target);e.stopPropagation();}, false);
+
+ pizarra.addEventListener("blur", () => {
+	  pul = false;}, false);
+
+ //  document.querySelector("main section:last-child").addEventListener("mouseup", e => {
+ //  	  pul = true; línea_actual = pizarra.lastChild; console.log(línea_actual,"section");}, false);
+}
+
+pizarra.addEventListener("paste", e => {
+	 e.preventDefault();
+	 poner_texto(e.clipboardData || window.clipboardData);}, false);
+
+pizarra.addEventListener("dragover", e => {
+	 e.preventDefault();}, false);
+
+cont_pizarra.addEventListener("drop", e => {
+	 e.preventDefault();
+	 pizarra.focus();
+	 poner_texto(e.dataTransfer);}, false);
+
+
+
+{let pintro = false, pback = false;
+
+ function metido_char(e){
+	  console.log("input",e);
+	 if(pintro) console.log(convertir_línea(línea_actual.textContent));
+	 //Comprobar si hay error y si lo hay poner marca roja en la línea.
+	  //const texto_línea = línea_actual.textContent;
+	  if(pback) poner_actual();//return;
+ } 
+
+ pizarra.addEventListener("keydown", e => {
+	  switch (e.key){
+	  case "Enter":
+			if(e.shiftKey) e.preventDefault();
+			else pintro = true;
+			break;
+	  case "Backspace":
+			pback = true;break;
+	  case "U+000A": console.log("lalala");e.preventDefault();
+	  }
+ }, false);
+
+ pizarra.addEventListener("input", metido_char, false);
+
+ pizarra.addEventListener("keyup", e => {
+	  if(e.key.indexOf("Arrow") != -1) {poner_actual();}
+	  else
+			switch (e.key){
+			case "PageDown":
+				 línea_actual = pizarra.lastChild;
+				 break;
+			case "PageUp":
+				 línea_actual = primera_línea;
+				 break;
+			case "Enter":
+				 pintro = false;
+				 //Comprobar si hay que indentar la línea. La línea anterior la analiza en input.
+				 break;
+			case "Backspace":
+				 pback = false;
+				 break;					 
+			}
+
+ });
+}
---
+++ js/interpretador.js
@@ -0,0 +1,139 @@
+const re_blancos = /\s/yug
+     ,re_coma = /,/yg
+     ,re_símbolo = /\s*(Any|None|not|::|sw|-(?:0|1)|\u0259|\.\.\.|\S)\s+/yug
+     ,re_griega = /\s*(?:[\u0381-\u03c9]|alpha|alfa|beta|gamma|delta|(?:e|é)psilon|(?:ds|z|th)?eta|iota|kappa|lambda|m(?:i|u)|n(?:u|i)|(?:ó|o)micron|rho|sigma|tau|(?:ú|i)psilon|(?:ph|f|ch|j|ps|x|p)i|omega)\s*/yg
+     ,re_acción = /\s*(R|L|P(\S)|E)\s*/yug
+     ,re_mconfig = /([a-zñáéíóú](?:[a-zA-ZñÑáéíóúÁÉÍÓÚ])*)(?:'|’)*([0-9])*(\(|\s)/yug
+     ,re_cualquiera = /\s*(\S+)/yug
+     ,re_varconfig = /\s*([A-ZÄÖÜ\uẞ])\s*/yg
+;
+
+const letras_griegas = {alfa:'\u03b1', alpha:'\u03b1', beta:'\u03b2', gamma:'\u03b3', delta:'\u03b4', epsilon:'\u03b5', épsilon:'\u03b5',
+								zeta:'\u03b6', dseta:'\u03b6', eta:'\u03b7', theta:'\u03b8', iota:'\u03b9', kappa:'\u03ba', lambda:'\u03bb', mu:'\u03bc',
+								mi:'\u03bc', nu:'\u03bd', ni:'\u03bd', xi:'\u03be',  ómicron:'\u03bf', omicron:'\u03bf', pi:'\u03c0', rho:'\u03c1',
+							   sigma:'\u03c3', tau:'\u03c4', úpsilon:'\u03c5', ipsilon:'\u03c5', fi:'\u03c6', phi:'\u03c6', ji:'\u03c7',
+								chi: '\u03c7', psi: '\u03c8', omega: '\u03c9'};
+
+
+const errores = [/*0*/"fin de lista de variables", /*1*/"variables en m-function", /*2*/"final de línea", /*3*/"m-function o espacio y símbolo"
+	,/*4*/"símbolo o llave de apertura", /*5*/"acciones o m-configuración final"];
+
+//Variable global que se pone cuando se detecta un error en la línea.
+var tok_esperado;
+    // ,índice;
+
+//Si hay espacios es una línea que empieza con símbolo y se llama a la función correspondiente; si no, empieza por mconfiguración.
+function int_línea(texto){
+	const partes = [];
+	var índice = 0;
+	tok_esperado = null;
+		
+	const espacios = aplicar_re(re_blancos); //re en vez de ver si hay un espacio para que funcione con cualquier espacio unicode.
+	if(!texto[índice]) return null; //No hay nada o sólo espacios.
+	if(texto[0] == "}"){
+		partes.push(new Llave("}", 6));
+		índice = 1;
+	}
+	else if(texto[0] == "{"){
+		partes.push(new Llave("{", 5));
+		índice = 1;
+	}
+	else{
+		if(espacios){
+			partes.push(espacios[0]);
+			emp_símbolo();
+		}
+		else emp_mconfig();
+	}
+
+	const ult_tok = aplicar_re(re_cualquiera);
+	const tok_error = ult_tok? ult_tok[1] : "";
+	if(tok_error && !tok_esperado) tok_esperado = errores[2];
+	const resto = tok_error + texto.slice(índice);
+	if(resto) partes.push(resto);
+	return partes;
+
+	function emp_mconfig(){
+		const mconf = int_mconfig();
+		if(!mconf) return;
+		partes.push(mconf);
+		if(tok_esperado) return;
+		const sig = buscar_símbolo();
+		if(!sig){
+			tok_esperado = errores[4];
+			return;
+		}
+		partes.push(sig);
+		const acc_mconf = int_acc_mconf();
+		if(acc_mconf) return; //Se puede seguir parseando.
+		if(sig.símbolo == "{"){ //No se puede seguir parseando pero si lo que se coge como símbolo es llave también es correcto.
+			partes.pop(); // Se quita el simbolo que se había metido y se interpreta como llave,
+			partes.push(new Llave(sig.texto, 5));
+			return;
+		}
+		tok_esperado = errores[5];
+	}
+
+	/*Devuelve un objeto. Primer parámetro el nombre del mconfig; segundo si acaba en número, para poder ponerlo como subíndice; tercero las variables; cuarto indica si
+	se ha termido de leer o lee parte.*/
+	function int_mconfig(){
+		var nombre = aplicar_re(re_mconfig);
+		if(!nombre){
+			tok_esperado = errores[3];
+			return null;
+		}
+		if(nombre[3] != "("){
+			índice--; //Para que vuelva a leer el espacio y guardar el nombre del símbolo sin este espacio haciendo trim.
+			return new Mconfig(nombre[1].trim(), nombre[2], [[],[]], false);
+		}
+		const variables = int_variables();
+		if(tok_esperado) return new Mconfig(nombre[1], nombre[2], variables, false);
+		if(texto[índice] == ")"){
+			índice++;
+			return new Mconfig(nombre[1], nombre[2], variables, true);
+		}
+		tok_esperado = errores[0];
+    	return new Mconfig(nombre[1], nombre[2], variables, false);
+	}
+
+	function int_variables(){
+		const vars = [[],[]];
+		var s;
+		var coma = true; // Para saber si ha salido del primer bucle por que no hay coma o porque no hay varconfig.
+		while(s = aplicar_re(re_varconfig)){
+			vars[0].push(s[0]);
+			if(!aplicar_re(re_coma)){
+				coma = false;
+				break;
+			}
+		}
+		if(coma)
+			while(s = aplicar_re(re_griega)){
+				vars[1].push(s[0]);
+				if(!aplicar_re(re_coma)) break;
+			}
+		if(!vars[0].length && !vars[1].length) tok_error = errores[1];
+		return vars;
+	}
+	
+	function aplicar_re(re){
+		re.lastIndex = índice;
+		var res = re.exec(texto);
+		if(índice < re.lastIndex) índice = re.lastIndex;
+		return res;
+	}
+}
+
+function convertir_línea(texto){
+	const df = document.createDocumentFragment();
+	const partes = int_línea(texto);
+	if(!partes) return null;
+	const n = partes.length;
+	for(let i=0;i<n;i++){
+		let nodos;
+		if(typeof partes[i] == "string") nodos = document.createTextNode(partes[i]);
+        else nodos = partes[i].obt_nodos();
+		df.appendChild(nodos);
+	}
+	return df;
+}
