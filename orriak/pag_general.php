24d90575398b8202f8200b0ac88736a0bb5a1122
rjcordoba <rcordoba77@yahoo.es>
2020-02-06 06:26:33 +0100

    05-02-20, Inicio responsive.

--- orriak/pag_general.php
+++ orriak/pag_general.php
@@ -7,47 +7,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="/css/general.css">
 	<?php foreach($estilos as $est): ?>
-		<link rel="stylesheet" type="text/css" href="/css/<?= $est ?>.css">
+	<link rel="stylesheet" type="text/css" href="/css/<?= $est ?>.css">
 	<?php endforeach ?>
 	<?php foreach($js as $js): ?>
-		<script defer src="/js/<?= $js ?>.js"></script>
+	<script defer src="/js/<?= $js ?>.js"></script>
 	<?php endforeach ?>
   </head>
   <body>
     <header>
 	  <div class="anchura">
-        <img src="/imgs/icono.svg" alt="icono">
-        <img src="/imgs/letras.svg" alt="letras">
+		<div id="logop">
+		  <img src="/imgs/icono.svg" alt="icono">
+		  <img src="/imgs/letras.svg" alt="letras">
+		</div>
 	  </div>
     </header>
-    <nav>
-	  <ul class="anchura">
-	    <li>
-		  <a href="./clenguaje.html">C--</a>
-		  <ul id="tab_c">
-			<li>lenguaje</li>
-			<li class="con_triang">macros<span class="triang">►</span></li>
-			<li>ejemplo Turing</li>
-			<li>ejemplo recursión</li>
-		  </ul>
-		</li>
-		<li>
-		  <a href="./index.html">Máquinas</a>
-		  <ul id="tab_amt">
-		  	<li>lenguaje</li>
-		  	<li>ejemplo C--</li>
-		  	<li>simulador</li>
-		  </ul>
-		</li>
-      </ul>
-    </nav>
-	<div id="cont-principal" class="anchura">
-	  <div id="encabezado">
-		<h1><?= $encabezado[0] ?></h1>
-		<h2><?= $encabezado[1] ?></h2>
-	  </div>
+	<div id="navegadorp">
+	  <div id="hamburguesa">☰</div>
+      <nav class="anchura">
+		<ul>
+	      <li>
+			<h1><a href="./es/cmm/clenguaje">C--</a></h1>
+			<ul>
+			  <li><a href="./es/cmm/clenguaje">lenguaje</a></li>
+			  <li class="con_triang"><a href="./es/cmm/macros/1">macros</a><span class="triang">►</span>
+				<ul>
+				  <li><a href="./es/cmm/macros/1">macros 1</a></li>
+				  <li><a href="./es/cmm/macros/1">macros 5</a></li>
+				  <li><a href="./es/cmm/macros/1">macros 9</a></li>
+				  <li><a href="./es/cmm/macros/1">macros 13</a></li>
+				  <li><a href="./es/cmm/macros/1">macros 19</a></li>
+				</ul>
+			  </li>
+			  <li><a href="./es/cmm/tmachine">ejemplo Turing</a></li>
+			  <li><a href="./es/cmm/recursive">ejemplo recursión</a></li>
+			</ul>
+		  </li>
+		  <li>
+			<h1><a href="./index.html">Máquinas</a></h1>
+			<ul>
+		  	  <li><a href="./es/amt/lenguaje">lenguaje</a></li>
+		  	  <li><a href="./es/amt/tmachine-cmm">ejemplo C--</a></li>
+		  	  <li class="por_hacer">simulador</li>
+			</ul>
+		  </li>
+		</ul>
+      </nav>
+	</div>
+	<div id="flex_central">
+	  <div id="cont-principal" class="anchura">
+		<div id="encabezado">
+		  <h1><?= $encabezado[0] ?></h1>
+		  <h2><?= $encabezado[1] ?></h2>
+		</div>
 		<?php include "$orria"; ?>
-    </div>
+	  </div>
+	</div>
     <footer>
 	  <div class="anchura">
 		<div id="autor">
